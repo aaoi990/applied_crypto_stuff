979143c785dd4e6da38aacac887c598a042ff17f
Amanda <a.thomson404@googlemail.com>
2021-02-24 22:10:46 +0000

    init key_exchange

---
+++ key_exchange/c/c3.py
@@ -0,0 +1,33 @@
+import random
+import base64
+import hashlib
+import sys
+
+g=5
+p=467
+
+a=random.randint(5, 10)
+
+b=random.randint(10,20)
+
+A = (g**a) % p
+B = (g**b) % p
+
+print(f"g = {g} (a shared value), n = {p} (a prime number)")
+print(f"Alice calculates:")
+print(f"a (Alice random): {a}")
+print(f"Alice value (A): {A} = (g^a) mod p")
+
+print(f"Bob calculates:")
+print(f"b (Bob random): {b}")
+print(F"Bob value (B): {B} = (g^b) mod p")
+
+print("Alice calculates:")
+keyA=(B**a) % p
+print(f"Key: {keyA} = (B^a) mod p")
+print(f"Key: {hashlib.sha256(str(keyA).encode()).hexdigest()}")
+
+print(f"Bob calculates:")
+keyB=(A**b) % p
+print(f"Key: {keyB} = (A^b) mod p")
+print(f"Key: {hashlib.sha256(str(keyB).encode()).hexdigest()}")
