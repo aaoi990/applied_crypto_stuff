488f04e5e2c01902198cf4fa6d4a2af6b2c9080a
Amanda <a.thomson404@googlemail.com>
2021-02-28 22:03:27 +0000

    More ecc

--- key_exchange/d/d2.py
+++ key_exchange/d/d2.py
@@ -1,29 +1,45 @@
 import os
 import binascii
-#from x25519 import base_point_mult,multscalar,bytes_to_int,int_to_bytes
-from cryptography.hazmat.primitives.asymmetric.x25519 import multscalar,bytes_to_int,int_to_bytes
+from x25519 import base_point_mult,multscalar
 
 a = os.urandom(32)
 b = os.urandom(32)
-# a = int_to_bytes(10,32) # just for testing a=10 (32 bytes - 256 bits)
-# b = int_to_bytes(12,32) # just for testing b=12 (32 bytes - 256 bits)
-
-print (f"\n\nBob private (b):\t{bytes_to_int(b)}")
-print (f"Alice private (a): \t{bytes_to_int(a)}")
 
 
-
-# Traditional ECDH: 
 a_pub = base_point_mult(a)
 b_pub = base_point_mult(b)
 
-print ("\n\nBob public (bG):\t",binascii.hexlify(b_pub.encode()))
+x = os.urandom(32)
+y = os.urandom(32)
 
-print ("Alice public (aG):\t",binascii.hexlify(a_pub.encode()))
-
-k_a = multscalar(a, b_pub) # a (bG)
-k_b = multscalar(b, a_pub) # b (aG)
+Bob_send = multscalar(y, a_pub) # (y) aG
+Bob_send = multscalar(b, Bob_send) # (yb) aG
 
 
-print ("\n\nBob shared (b)aG:\t",binascii.hexlify(k_b.encode()))
-print ("Alice shared (a)bG:\t",binascii.hexlify(k_a.encode()))
+Alice_send = multscalar(x, b_pub) # (x) bG
+Alice_send = multscalar(a, Alice_send) # (xa) bG
+
+
+k_a = multscalar(x, Bob_send) # x (yb) aG
+k_b = multscalar(y, Alice_send) # y ( xa) bG
+
+
+print ("Bob private:\t",binascii.hexlify(a))
+print ("Alice private:\t",binascii.hexlify(b))
+
+print ("\n\nBob public:\t",binascii.hexlify(b_pub.encode()))
+print ("Alice public:\t",binascii.hexlify(a_pub.encode()))
+
+print ("\nBob x value:\t",binascii.hexlify(x))
+print ("Alice y value:\t",binascii.hexlify(y))
+
+print ("\n\nBob send:\t",binascii.hexlify(Bob_send.encode()))
+print ("Alice send:\t",binascii.hexlify(Alice_send.encode()))
+
+print ("\n\nBob shared:\t",binascii.hexlify(k_b.encode()))
+print ("Alice shared:\t",binascii.hexlify(k_a.encode()))
+
+# res=bytes_to_int(a)*bytes_to_int(y)*bytes_to_int(b)*bytes_to_int(x)
+
+# k = base_point_mult(int_to_bytes(res,32))
+# print ("\n\nChecking shared:\t",binascii.hexlify(k.encode()))
