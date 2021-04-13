11e913d4d238096ce58891c86b4ad1940c0c05b4
amanda <a.thomson404@googlemail.com>
2021-04-13 20:41:40 +0100

    Working

--- coursework/2.js
+++ coursework/2.js
@@ -1,25 +1,18 @@
 /* eslint-disable no-console */
 'use strict'
 
-const Libp2p = require('../../')
+const Libp2p = require('libp2p')
 const TCP = require('libp2p-tcp')
+const uint8ArrayFromString = require('uint8arrays/from-string')
 const Mplex = require('libp2p-mplex')
 const { NOISE } = require('libp2p-noise')
 const crypto = require('crypto')
 const CID = require('cids')
 const multihash = require('multihashes')
 const KadDHT = require('libp2p-kad-dht')
-const Bootstrap = require('libp2p-bootstrap')
-
-
-const all = require('it-all')
+const utils = require('libp2p-kad-dht/src/utils')
 const delay = require('delay')
-
-const bootstrapMultiaddrs = [
-  '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-  '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN'
-]
-
+const distance = require('xor-distance')
 
 const createNode = async () => {
   const node = await Libp2p.create({
@@ -33,51 +26,90 @@ const createNode = async () => {
       dht: KadDHT,
     },
     config: {
-      dht: {
-        enabled: true
+      dht: {                        // The DHT options (and defaults) can be found in its documentation
+        kBucketSize: 20,
+        enabled: true,
+        randomWalk: {
+          enabled: false,            // Allows to disable discovery (enabled by default)
+          interval: 300e3,
+          timeout: 10e3
+        }
       }
     }
   })
-
   await node.start()
   return node
 }
 
-;(async () => {
-  const [node1, node2, node3] = await Promise.all([
-    createNode(),
-    createNode(),
-    createNode()
-  ])
-
-  node1.peerStore.addressBook.set(node2.peerId, node2.multiaddrs)
-  node2.peerStore.addressBook.set(node3.peerId, node3.multiaddrs)
-
-  await Promise.all([
-    node1.dial(node2.peerId),
-    node2.dial(node3.peerId)
-  ])
-
-  // Wait for onConnect handlers in the DHT
-  await delay(100)
-
-  const hash = crypto.createHash('sha256').update('hello word!').digest()
+const create_CID = (content) => {
+  const hash = crypto.createHash('sha256').update(content).digest()
   const encoded = multihash.encode(hash, 'sha2-256')
   let cid = new CID(multihash.toB58String(encoded))
-  console.log('CID:', cid)
+  return cid
+}
 
-  await node1.contentRouting.provide(cid)
-  await node2.contentRouting.provide(cid)
+const map_nodes_to_peerids = (nodes) => {
+  let node_mappings = []
+  let node_peerIds = []
+  nodes.forEach((node) => {
+    node_mappings.push(node.peerId.toB58String())
+    node_peerIds.push(node.peerId)
+  })
+  return { node_mappings, node_peerIds }
+}
 
-  console.log('Node %s is providing %s', node1.peerId.toB58String(), cid.toBaseEncodedString())
+const sort_closest_nodes = async (peers, query, node_mappings) => {
+  let query_key = uint8ArrayFromString(query)
+  let dhtkey = await utils.convertBuffer(query_key)
+  let sorted_peers = await utils.sortClosestPeers(node_mappings, dhtkey)
+  let closest_peers = []
+  for await (let sorted of sorted_peers) {
+    let peer = peers.findIndex(element => element.peerId.toB58String() == sorted._idB58String)
+     closest_peers.push(peer)
+  }
+  return closest_peers
+}
+
+;(async () => {
+  const nodes = await Promise.all([
+    createNode(), createNode(), createNode(), createNode(), createNode(), createNode(), createNode(), createNode(), createNode(), createNode()
+  ])
+
+  for(let index = 0; index < nodes.length - 1; index++) {
+    nodes[index].peerStore.addressBook.set(nodes[index + 1].peerId, nodes[index + 1].multiaddrs)
+  }
+
+  await Promise.all([
+    nodes[0].dial(nodes[1].peerId),
+    nodes[1].dial(nodes[2].peerId)
+  ])
+
+  const { node_mappings, node_peerIds } = map_nodes_to_peerids(nodes)
+  //console.log(node_mappings, node_peerIds)
+  // Wait for onConnect handlers in the DHT
+  await delay(10000)
+
+
+  let cid = create_CID("hello world!")
+  await nodes[0].contentRouting.provide(cid)
+
+  console.log('Node %s is providing %s', nodes[0].peerId.toB58String(), cid.toBaseEncodedString())
 
   // wait for propagation
-  await delay(300)
 
-  const providers = await all(node3.contentRouting.findProviders(cid, { timeout: 3000 }))
+  const closest = await sort_closest_nodes(nodes, 'hello world!', node_peerIds)
+  console.log("Peers that are closest to the target provider: ")
+  for(let index = 0; index < closest.length; index++) {
+    console.log("Node ", index + " : " + node_mappings[index])
+  }
 
-  console.log('Found provider:', providers[0].id.toB58String(), providers)
-  node1.stop()
-  node2.stop()
-  node3.stop()
+
+
+  //const providers = await all(nodes[2].contentRouting.findProviders(cid, { timeout: 3000 }))
+
+  //console.log('Found provider:', providers[0].id.toB58String(), providers)
+
+  // nodes.forEach((node) => {
+  //   node.stop()
+  // })
 })();
